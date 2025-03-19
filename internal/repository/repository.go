4de8660c3b28bc626271ef45d01cbfbada22ae74
Denis Bochko <reElcy@yandex.ru>
2025-03-19 11:09:05 +0300

    added repository with postgresql

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,6 +1,10 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	test "order-server/pkg/api"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type Repository struct {
 	DB *pgxpool.Pool
@@ -8,4 +12,27 @@ type Repository struct {
 
 func New(db *pgxpool.Pool) *Repository {
 	return &Repository{DB: db}
+}
+
+func (r *Repository) Create(item string, quantity int32) string {
+	return ""
+}
+
+func (r *Repository) Update(id string, item string, quantity int32) (*test.Order, error) {
+	var order *test.Order
+	return order, nil
+}
+
+func (r *Repository) Get(id string) (*test.Order, error) {
+	var order *test.Order
+	return order, nil
+}
+
+func (r *Repository) Delete(id string) (bool, error) {
+	return true, nil
+}
+
+func (r *Repository) List() []*test.Order {
+	orders := make([]*test.Order, 0, 0)
+	return orders
 }
