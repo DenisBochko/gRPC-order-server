f543a5a2e5dfd9a44e3e5d5e831be360edfe304f
Denis Bochko <reElcy@yandex.ru>
2025-03-20 23:05:02 +0300

    patterns: update

--- pkg/patterns/patterns.go
+++ pkg/patterns/patterns.go
@@ -1,1 +1,12 @@
 package patterns
+
+import "fmt"
+
+func Retry(fn func() error, maxRetries int, baseDelay int) error {
+	x := fn()
+	return x
+}
+
+func errorFunction() error {
+	return fmt.Errorf("Ошибка!")
+}
